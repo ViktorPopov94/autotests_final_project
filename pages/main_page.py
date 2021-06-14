93137689308194449c28367065adafad56ec23ea
Viktor <Popov-VV94@yandex.ru>
2021-06-14 12:10:21 +0500

    base_page, main_page, test_main_page: добавлена проверка существования элемента на странице

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,10 @@
-from .base_page import Basepage
+from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
-class MainPage(Basepage):
+class MainPage(BasePage):
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Элемент не найден"
+
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
