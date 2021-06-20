eaa96a8062a7e81d6409a0359a82c46741f0f911
Viktor <Popov-VV94@yandex.ru>
2021-06-20 12:43:16 +0500

    Product page изменён под проверку разных ссылок и поиск упавшего теста

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
+import pytest
 #col-sm-6 product_main
 #alertinner
 class ProductPage(BasePage):
@@ -12,19 +13,14 @@ class ProductPage(BasePage):
         book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
         added_book_name = self.browser.find_element(*ProductPageLocators.ADDED_BOOK_NAME).text
 
+        assert book_name == added_book_name, 'Названия выбранного и добавленного товаров не совпадают'
         if book_name == added_book_name:
             print('Товар добавлен в корзину. Названия выбранного и добавленного товаров совпадают')
-        else:
-            print('Названия выбранного и добавленного товаров не совпадают')
 
     def check_of_price_of_book(self):  # Проверка совпадения стоимости выбранного и добавленного товаров
         book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
         added_book_price = self.browser.find_element(*ProductPageLocators.ADDED_BOOK_PRICE).text
 
+        assert book_price == added_book_price, "Стоимость выбранного товара не соответствует стоимости добавленного товара"
         if book_price == added_book_price:
             print('Стоимость выбранного товара соответствует стоимости добавленного товара')
-        else:
-            print('Стоимость выбранного товара не соответствует стоимости добавленного товара')
-
-
-
--- test_product_page.py
+++ test_product_page.py
@@ -1,14 +1,28 @@
 #pytest -v --tb=line --language=en test_product_page.py
 # pytest -s test_product_page.py
+# pytest -rX -v test_product_page.py
 from .pages.product_page import ProductPage
-from .pages.base_page import BasePage
+import pytest
 import time
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
+#@pytest.mark.xfail
+def test_guest_can_add_product_to_basket(browser, link):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5"
     page = ProductPage(browser, link)
     page.open()
     page.click_to_add_to_basket()
     page.solve_quiz_and_get_code()  # решение мат.задачи 4.3.2
     page.check_of_name_of_book()  # Проверка совпадения выбранного и добавленного товаров
     page.check_of_price_of_book()  # Проверка совпадения стоимости выбранного и добавленного товаров
+
